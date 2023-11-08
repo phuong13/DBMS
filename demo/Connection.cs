b8e506162fa0a04941d1cea4f4e8edaeae7c6294
phucth071 <phucth0710@gmail.com>
2023-11-08 20:43:38 +0700

    Modify Connection.cs

--- demo/Connection.cs
+++ demo/Connection.cs
@@ -8,40 +8,60 @@ using System.Threading.Tasks;
 
 namespace demo
 {
-   public class Connection
+    public class Connection
     {
-
-        SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=ThietBiViTinh;Integrated Security=True");
+        SqlConnection cnn = new SqlConnection("Data Source=.;Initial Catalog=ThietBiViTinh;Persist Security Info=True;User ID=sa;Password=1234567890");
         public SqlConnection getConnection
         {
             get
             {
-                return conn;
+                return cnn;
+            }
+        }
+        SqlConnection cnnAdmin = new SqlConnection("Data Source=.;Initial Catalog=ThietBiViTinh;Integrated Security=True");
+        public SqlConnection getConnectionAdmin
+        {
+            get
+            {
+                return cnnAdmin;
             }
         }
 
         public void openConnection()
         {
-            if (conn.State == ConnectionState.Closed)
+            if (cnn.State == ConnectionState.Closed)
             {
-                conn.Open();
+                cnn.Open();
+            }
+        }
+        public void openConnectionAdmin()
+        {
+            if (cnnAdmin.State == ConnectionState.Closed)
+            {
+                cnnAdmin.Open();
             }
         }
 
         public void closeConnection()
         {
-            if (conn.State == ConnectionState.Open)
+            if (cnn.State == ConnectionState.Open)
             {
-                conn.Close();
+                cnn.Close();
+            }
+        }
+        public void closeConnectionAdmin()
+        {
+            if (cnnAdmin.State == ConnectionState.Open)
+            {
+                cnnAdmin.Close();
             }
         }
         public DataTable getTable(string sql)
         {
             DataTable dt = new DataTable();
-            SqlDataAdapter ds = new SqlDataAdapter(sql, conn);
+            SqlDataAdapter ds = new SqlDataAdapter(sql, cnn);
             ds.Fill(dt);
             return dt;
         }
     }
 }
-
